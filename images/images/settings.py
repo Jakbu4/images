bb8cecf15ef2589ea10707d4ccb52016171d28f1
Jakub Bednarz <bednaszjakub@gmail.com>
2022-06-05 12:45:18 +0200

    docker configuration

--- images/images/settings.py
+++ images/images/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'images.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'images',
+        'USER': 'images',
+        'PASSWORD': 'images',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
